e222a0bfaac953daf4c0af9a13da951f3ae7832e
wth153 <126817920+wth153@users.noreply.github.com>
2023-03-27 11:31:18 +0200

    Update udrive.py

--- scripts/udrive.py
+++ scripts/udrive.py
@@ -28,11 +28,11 @@ try:
     from baidupcs_py.baidupcs import BaiduPCSApi
 except ImportError:
     if not launch.is_installed("baidupcs_py"):
-        launch.run_pip("install BaiduPCS-Py", "baidupcs_py")
+        launch.run_pip("install BaiduPCS-Py --no-deps", "baidupcs_py --no-deps")
         from baidupcs_py.baidupcs import BaiduPCSApi
         
-subprocess.run("curl -o /usr/local/lib/python3.9/dist-packages/uvicorn/loops/auto.py  https://raw.githubusercontent.com/encode/uvicorn/master/uvicorn/loops/auto.py", shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-subprocess.run("curl -o /usr/local/lib/python3.9/dist-packages/uvicorn/loops/uvloop.py  https://raw.githubusercontent.com/encode/uvicorn/master/uvicorn/loops/uvloop.py", shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+#subprocess.run("curl -o /usr/local/lib/python3.9/dist-packages/uvicorn/loops/auto.py  https://raw.githubusercontent.com/encode/uvicorn/master/uvicorn/loops/auto.py", shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+#subprocess.run("curl -o /usr/local/lib/python3.9/dist-packages/uvicorn/loops/uvloop.py  https://raw.githubusercontent.com/encode/uvicorn/master/uvicorn/loops/uvloop.py", shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 
 class udrive:
     def __init__(self, ):
